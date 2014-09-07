205856f9fdff28f04183c579f41301d7ebeaa977
nimadadgar <nima@dadgar.org>
2014-09-08 00:10:33 +1000

    Header with menu working

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
-﻿$( document ).ready(function() {
-    $("button").click(function () {
-        $("p").toggle();
+﻿$(document).ready(function () {
+    $(".burger").click(function () {
+        $(".main-menu-list").toggle("slow");
     });
+});
+
+$(window).resize(function () {
+    if ($(window).width() >= 992) {
+        $(".main-menu-list").show();
+    } else if ($(window).width() < 991) {
+        $(".main-menu-list").hide();
+    }
 });
