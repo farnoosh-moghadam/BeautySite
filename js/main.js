7a21fe6d4e22891ca701ef63b872c2e1d480268b
nimadadgar <nima@dadgar.org>
2014-09-21 16:07:45 +1000

    learing arrow animation added

--- js/main.js
+++ js/main.js
@@ -1,62 +1,71 @@
 ﻿var isMobile = false;
-	
+
 $(document).ready(function () {
-	if ($(window).width() >= 992) {
-		isMobile = false;
-	}
-	else{
-		isMobile = true;
-	}
-	
+    if ($(window).width() >= 992) {
+        isMobile = false;
+    }
+    else {
+        isMobile = true;
+    }
+
     $('#burger').click(function () {
         $('.main-menu-list').toggle('slow');
     });
-	
-	var selectors = [];
-	selectors.push('#BeautyTherapy');
-	selectors.push('#MakeUp');
-	selectors.push('#Nail');
 
-	$(selectors).each(function(index, element) {
-			$(element + ' .panel-heading a').on('click', function(e) {
-				if($(e.target).parents('.panel-heading').next('.panel-collapse').hasClass('in') && !isMobile){
-					e.preventDefault();
-					e.stopPropagation();
-				}
-			});
-			
-		  	$(element).on('show.bs.collapse', function (e) {
-				  $(e.target).prev('.panel-heading').find('.icon').removeClass('icon-plus').addClass('icon-minus');
-			   });
-			   
-		  	$(element).on('hidden.bs.collapse', function (e) {
-			  	  $(e.target.parentNode).not($(e.target).prev('.panel-heading')).find('.icon').removeClass('icon-minus').addClass('icon-plus');
-			});  
-	});
+    var selectors = [];
+    selectors.push('#BeautyTherapy');
+    selectors.push('#MakeUp');
+    selectors.push('#Nail');
 
-	if (isMobile) {
-	    $(".collapse").each(function (index, element) {
-	        $(element).removeClass('in').prev('.panel-heading').find('.icon').removeClass('icon-minus').addClass('icon-plus');
-	    });
-	}
+    $(selectors).each(function (index, element) {
+        $(element + ' .panel-heading a').on('click', function (e) {
+            if ($(e.target).parents('.panel-heading').next('.panel-collapse').hasClass('in') && !isMobile) {
+                e.preventDefault();
+                e.stopPropagation();
+            }
+        });
 
-	$("#myTab a").each(function (index, element) {
-	    $(element).click(function (e) {
-	        e.preventDefault();
-	        $(this).tab('show');
-	    });
-	});
+        $(element).on('show.bs.collapse', function (e) {
+            $(e.target).prev('.panel-heading').find('.icon').removeClass('icon-plus').addClass('icon-minus');
+        });
+
+        $(element).on('hidden.bs.collapse', function (e) {
+            $(e.target.parentNode).not($(e.target).prev('.panel-heading')).find('.icon').removeClass('icon-minus').addClass('icon-plus');
+        });
+    });
+
+    if (isMobile) {
+        $(".collapse").each(function (index, element) {
+            $(element).removeClass('in').prev('.panel-heading').find('.icon').removeClass('icon-minus').addClass('icon-plus');
+        });
+    }
+
+    $("#myTab a").each(function (index, element) {
+        $(element).click(function (e) {
+            e.preventDefault();
+            $(this).tab('show');
+            $("#learnArrow").animate({
+                "margin-left": $(this).position().left + $(this).width() / 2 - 24,
+            });
+            $("#learnArrow").removeClass(function (index, css) {
+                return (css.match(/\bborder-\S+/g) || []).join(' ');
+            }).addClass('border-' + $(this).attr("color"));
+            $("#learnTabContent").removeClass(function (index, css) {
+                return (css.match(/\blearningProcess-box-\S+/g) || []).join(' ');
+            }).addClass('learningProcess-box-' + $(this).attr("color"));
+        });
+    });
 });
 
 $(window).resize(function () {
     if ($(window).width() >= 992) {
-		isMobile = false;
+        isMobile = false;
         $(".main-menu-list").show();
     } else {
-		isMobile = true;
-		$(".main-menu-list").hide();
-		$(".collapse").each(function (index, element) {
-		    $(element).removeClass('in').prev('.panel-heading').find('.icon').removeClass('icon-minus').addClass('icon-plus');
-		});
+        isMobile = true;
+        $(".main-menu-list").hide();
+        $(".collapse").each(function (index, element) {
+            $(element).removeClass('in').prev('.panel-heading').find('.icon').removeClass('icon-minus').addClass('icon-plus');
+        });
     }
 });
