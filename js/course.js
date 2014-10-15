f6d1809c513f57a717e50e1abc23b24f51d2339e
nimadadgar <nima@dadgar.org>
2014-10-16 00:24:07 +1100

    sticky form

---
+++ js/course.js
@@ -0,0 +1,33 @@
+﻿function handleSticky(dir) {
+    if ($(window).width() < 992) {
+        return;
+    }
+    if (dir === 'down') {
+        $('#stickyForm').find('.form-course-box').css({ 'margin-top': '0px','width':'262px' });
+    }
+    else {
+        $('#stickyForm').find('.form-course-box').css({ 'margin-top': '-150px', 'width': '100%' });
+    }
+}
+
+$(document).ready(function () {
+
+    if ($(window).width() >= 992) {
+        $('#stickyForm').waypoint('sticky', {
+            handler: handleSticky,
+        });
+    }
+});
+
+$(window).resize(function () {
+    if ($(window).width() >= 992) {
+        $('#stickyForm').waypoint('unsticky');
+        $('#stickyForm').waypoint('sticky', {
+            handler: handleSticky,
+        });
+    }
+    else
+    {
+        $('#stickyForm').waypoint('unsticky');
+    }
+});
