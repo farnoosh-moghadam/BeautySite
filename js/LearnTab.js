a4498208c956ff5a526ca8408e66cc50aba97db1
nimadadgar <nima.dadgar@objectconsulting.com.au>
2014-11-09 23:29:24 +1100

    learn tab
    h1
    contact

--- js/LearnTab.js
+++ js/LearnTab.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     $("#myTab a").each(function (index, element) {
         $(element).click(function () {
             $('html, body').animate({
-                scrollTop: $(".tab-content").offset().top-100
+                scrollTop: $("#myTab").offset().top -50
             }, 1000);
 
             $(this).tab('show');
